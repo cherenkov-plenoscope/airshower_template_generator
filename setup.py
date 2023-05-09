47805e3e8ca6acb2fffc07f60b55cc5aab98d9b3
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-05-09 23:13:28 +0200

    bumb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="airshower_template_generator",
-    version="0.2.0",
+    version="0.3.0",
     description="Generate Cherenkov-light-templates of cosmic-ray airshowers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
