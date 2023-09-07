54bfb08aa771ee36dc6f4463f6fd3f9c9e203cfd
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-09-07 21:11:58 +0200

    adopt 2023-flow

---
+++ README.rst
@@ -0,0 +1,27 @@
+############################
+Airshower-Template-Generator
+############################
+|TestStatus| |PyPiStatus| |BlackStyle|
+
+|FigImageOfShower|
+
+Generate Cherenkov-light-templates of airshowers induced by cosmic-rays. Create a look-up-table binned in:
+
+- Energy of primary particle
+- Altitude of airshower's maximum
+- Azimuth of observer w.r.t. airshower-axis
+- Radial distance of observer w.r.t. airshower-axis
+
+This uses KIT's CORSIKA to simulate the Cherenkov-emission in airshowers.
+
+
+.. |BlackStyle| image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+
+.. |TestStatus| image:: https://github.com/cherenkov-plenoscope/airshower_template_generator/actions/workflows/test.yml/badge.svg?branch=main
+   :target: https://github.com/cherenkov-plenoscope/airshower_template_generator/actions/workflows/test.yml
+
+.. |PyPiStatus| image:: https://img.shields.io/pypi/v/airshower_template_generator_cherenkov-plenoscope
+   :target: https://pypi.org/project/airshower_template_generator_cherenkov-plenoscope/
+
+.. |FigImageOfShower| image:: https://github.com/cherenkov-plenoscope/airshower_template_generator/blob/main/airshower_template_generator/readme/view.jpg?raw=True
--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 import setuptools
 import os
 
-with open("README.md", "r") as f:
+
+with open("README.rst", "r") as f:
     long_description = f.read()
 
+
+with open(os.path.join("airshower_template_generator", "version.py")) as f:
+    txt = f.read()
+    last_line = txt.splitlines()[-1]
+    version_string = last_line.split()[-1]
+    version = version_string.strip("\"'")
+
+
 setuptools.setup(
-    name="airshower_template_generator",
-    version="0.3.2",
+    name="airshower_template_generator_cherenkov-plenoscope",
+    version=version,
     description="Generate Cherenkov-light-templates of cosmic-ray airshowers.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/cherenkov-plenoscope/airshower_template_generator.git",
     author="Sebastian Achim Mueller",
     author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
     packages=["airshower_template_generator",],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Natural Language :: English",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
+    package_data={"airshower_template_generator": []},
     install_requires=[
         "json_utils_sebastian-achim-mueller",
         "binning_utils_sebastian-achim-mueller",
@@ -32,5 +33,14 @@ setuptools.setup(
         "plenoirf",
         "network_file_system_sebastian-achim-mueller",
         "scipy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Astronomy",
     ],
 )
